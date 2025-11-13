53e570ebbf8adc6b7131bf5be0cdacae82c24904
Gabriel Costa Vianna <gabrielvianna495@gmail.com>
2025-11-13 12:04:22 -0300

    Solução aproximada para o problema dos K-centros - Usando o algoritmo de gonzales como referencia

--- src/ApproximateSolver.java
+++ src/ApproximateSolver.java
@@ -1,9 +1,71 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ApproximateSolver implements IsSolver{
 
     @Override
-    public Solution solve(int k, int v, double[][] matrizDisntancia) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Solution solve(InstanceData instancia) {
+
+     List<Integer> centros = new ArrayList<Integer>();
+     
+     //Inicializa o primeiro vértice do grafo como sendo o primeiro dos centros
+     centros.add(0);
+
+     for(int i = 0; i < (instancia.getK() - 1); i++){
+
+        double maiorDistancia = -1;
+        int verticeMaiorDistancia = -1;
+
+        for(int j = 0; j < instancia.getV(); j++){
+        
+         double distanciaMinima_do_j = Double.POSITIVE_INFINITY;
+         
+            for(int centro : centros){
+                double dist = instancia.getMatriz()[j][centro];
+
+                if (dist < distanciaMinima_do_j) {
+                    distanciaMinima_do_j = dist;
+                    }
+             }     
+             
+             if(distanciaMinima_do_j > maiorDistancia){
+                maiorDistancia = distanciaMinima_do_j;
+                verticeMaiorDistancia = j;
+             }
+        }
+
+        centros.add(verticeMaiorDistancia);
+     }
+  
+     double raio = calcularRaio(centros, instancia);
+
+     return new Solution(centros, raio);
+     
+ }
+    
+
+   private double calcularRaio(List<Integer> centros, InstanceData instancia){
+ 
+    double raioFinal = 0.0;
+
+    for(int i = 0; i < instancia.getV(); i++){
+        double distanciaMinima_do_v = Double.POSITIVE_INFINITY;
+
+        for(int centro : centros){
+            double dist = instancia.getMatriz()[i][centro];
+
+            if(dist < distanciaMinima_do_v){
+                distanciaMinima_do_v = dist;
+            }
+        }
+
+        if (distanciaMinima_do_v > raioFinal) {
+            raioFinal = distanciaMinima_do_v;
+        }
     }
-    
-    
+
+    return raioFinal;
+}  
+
 }
